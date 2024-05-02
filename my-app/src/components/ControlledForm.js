0a4a5b6cff913f1b894ab12fa8d569df4a74649c
Martin Bradbury <martinbradbury@btinternet.com>
2024-05-02 11:40:55 +0000

    added a category select and comment section to the form and enabled react to make it and not the DOM

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,9 +5,29 @@ export class ControlledForm extends Component {
       super(props)
     
       this.state = {
-         name: ""
+         name: "",
+         category: "Website",
+         comments: ""
       }
     }
+
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    };
+
+    handleCommentChange = (event) => {
+        this.setState({
+            comments: event.target.value,
+        });
+    };
+
+    handleCategoryChange = (event) => {
+        this.setState({
+            category: event.target.value,
+        });
+    };
 
 
 
@@ -17,13 +37,38 @@ export class ControlledForm extends Component {
   render() {
     return (
       <div>
+        <h2>Please Fill Out the Form Below</h2>
         <form>
-            <label htmlFor="id-name">Your Name:</label>
-            <input
-                value={this.state.name} 
-                id="id-name" name="name" 
-                type="text"/>
-            
+            <div>
+                <label htmlFor="id-name">Your Name:</label>
+                <input
+                    value={this.state.name}
+                    onChange={this.handleNameChange} 
+                    id="id-name" name="name" 
+                    type="text"/>
+            </div>
+            <div>
+                <select 
+                    htmlFor="id-category" 
+                    name="category"
+                    value={this.state.category}
+                    onChange={this.handleCategoryChange}
+                >
+                    <option value="Website">Website Issue</option>
+                    <option value="Order">Order Issue</option>
+                    <option value="General">General Issue</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="id-comments">Comments:</label>
+                <textarea 
+                    id="id-comments" 
+                    name="comments" 
+                    value={this.state.comments} 
+                    onChange={this.handleCommentChange}
+                />
+            </div>
+
             <input type="submit" value="submit"></input>
         </form>
       </div>
