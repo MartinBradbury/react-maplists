7581e2b3e042a4ceb4cd86cd1cfb4195f70bb225
Martin Bradbury <martinbradbury@btinternet.com>
2024-05-13 21:44:21 +0000

    added a useEffect to only render when object is mounted and not an every state change.

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -3,9 +3,15 @@ import React, {useState, useEffect} from 'react'
 function UseEffectCounter() {
     
     const [count, setCount] = useState(0);
+    const [timer, setTimer] = useState(0);
     useEffect(() => {
         document.title = count;
     }, [count]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer(time => time +1)
+        }, 1000);
+    },[])
     // const click = () => {
     //     setCount(count => count + 1)
     // };
@@ -17,6 +23,8 @@ function UseEffectCounter() {
             setCount(count => count +1)
         }}>
             this is the count: {count}</button>
+        
+        <h2>Timer: {timer}</h2>
     </div>
   )
 }
